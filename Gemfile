0ce2c7dcee2aa78e5315f16052912c5c0fa4ab3c
Alexandre Jose Dutra Gomes <agomes@tray.net.br>
2015-11-11 15:04:38 -0200

    Initial Commit

---
+++ Gemfile
@@ -0,0 +1,17 @@
+source "https://rubygems.org"
+
+gem "lita"
+
+# Uncomment to use the HipChat adapter
+# gem "lita-hipchat"
+
+# Uncomment to use the IRC adapter
+# gem "lita-irc"
+
+# Add handlers to give Lita new functionality.
+# For example:
+# gem "lita-google-images"
+# gem "lita-karma"
+#
+
+gem "lita-slack"
---
+++ lita_config.rb
@@ -0,0 +1,39 @@
+Lita.configure do |config|
+  config.robot.name = "Lita"
+  config.robot.log_level = :debug
+  config.robot.adapter = :slack
+  config.adapters.slack.token = ""
+  config.redis[:host] = "redis"
+
+  # The name your robot will use.
+  #config.robot.name = "Lita"
+
+  # The locale code for the language to use.
+  # config.robot.locale = :en
+
+  # The severity of messages to log. Options are:
+  # :debug, :info, :warn, :error, :fatal
+  # Messages at the selected level and above will be logged.
+  #config.robot.log_level = :info
+
+  # An array of user IDs that are considered administrators. These users
+  # the ability to add and remove other users from authorization groups.
+  # What is considered a user ID will change depending on which adapter you use.
+  # config.robot.admins = ["1", "2"]
+
+  # The adapter you want to connect with. Make sure you've added the
+  # appropriate gem to the Gemfile.
+  #config.robot.adapter = :shell
+
+  ## Example: Set options for the chosen adapter.
+  # config.adapter.username = "myname"
+  # config.adapter.password = "secret"
+
+  ## Example: Set options for the Redis connection.
+  # config.redis.host = "127.0.0.1"
+  # config.redis.port = 1234
+
+  ## Example: Set configuration for any loaded handlers. See the handler's
+  ## documentation for options.
+  # config.handlers.some_handler.some_config_key = "value"
+end
